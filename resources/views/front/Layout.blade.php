ee3aff72c6ad6f47b4951152b7399f523d3b3623
Untara Vivi Chahya <vyvy1777@gmail.com>
2024-04-21 11:29:07 +0700

    prod setup

--- resources/views/front/Layout.blade.php
+++ resources/views/front/Layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @if (ENV('APP_ENV') == 'local')
+    @if (ENV('APP_ENV') != 'local')
         @include('front.components.StyleVersion')
     @else
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
